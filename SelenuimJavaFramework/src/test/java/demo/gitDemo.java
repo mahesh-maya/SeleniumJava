03ce4707342c149ceb58938aa5f0f19d85d150d4
Mahesh <maheshgadekar1994@gmail.com>
2020-12-25 12:08:44 +0530

    adding class in develop branch

---
+++ SelenuimJavaFramework/src/test/java/demo/gitDemo.java
@@ -0,0 +1,23 @@
+package demo;
+
+public class gitDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Git Testing");
+		System.out.println("Testing");
+		
+		test1();
+		test2();
+
+	}
+	
+public static void test1() {
+	System.out.println("test 1");
+}
+
+public static void test2() {
+	System.out.println("test 2");
+}
+
+}
